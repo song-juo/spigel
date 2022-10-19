c1a4c445abf32871f3980a0a78072515eaf1c4ef
George Santos <47012804+myur4@users.noreply.github.com>
2022-10-19 01:08:30 -0300

    Algorithms updated and padronized into semelliant classes for transparency

--- src/structures/dHash.ts
+++ src/structures/dHash.ts
@@ -1,100 +1,99 @@
 import sharp from 'sharp';
-import fs from 'fs';
 import bigInt from 'big-integer';
+import {readFileSync} from 'fs';
+
+import type {ComparisonOptions} from '../typing';
+import type {DistanceResult} from '../typing/structures';
 
 /* This code implements the "dHash" algorithm described in: https://www.hackerfactor.com/blog/?/archives/529-Kind-of-Like-That.html */
 
-interface ComparisonOptions {
-    verbose: boolean
-    hashes: boolean
-}
+export default class DifferencialHash {
+	private readonly width: number;
+	private readonly height: number;
 
-interface ComparisonResult {
-    distance: number
-    hashes: { image: string, image2: string }
-}
+	constructor() {
+		this.width = 9;
+		this.height = 8;
+	}
 
-export default class dHash {
+	public async execute(imagePath: string | Buffer): Promise<string> {
+		const difference = await this._difference(imagePath);
 
-    private width: number;
-    private height: number;
+		let decimalValue = 0;
+		let hashString = '';
 
-    constructor() {
-        this.width = 9;
-        this.height = 8;
-    }
+		for (const [index, value] of difference.entries()) {
+			const numericValue = value ? 1 : 0;
 
-    public async calculate(imagePath: string | Buffer): Promise<string> {
-        const difference = await this._difference(imagePath)
+			if (value) {
+				decimalValue += numericValue * (2 ** (index % 8));
+			}
 
-        let decimalValue = 0
-        let hashString = ""
+			if (index % 8 === 7) {
+				const parsedHex = decimalValue.toString(16).slice(-2).padStart(2, '0');
+				hashString += parsedHex;
+				decimalValue = 0;
+			}
+		}
 
-        for (const [index, value] of difference.entries()) {
+		return hashString;
+	}
 
-            const numericValue = value ? 1 : 0;
+	public compareHash(hash: string, hash2: string): number {
+		const calcHash = this._hex2Dec(hash);
+		const calcHash2 = this._hex2Dec(hash2);
 
-            if (value) {
-                decimalValue += numericValue * (2 ** (index % 8))
-            }
-            if (index % 8 === 7) {
-                let parsedHex = decimalValue.toString(16).slice(-2).padStart(2, "0")
-                hashString += parsedHex
-                decimalValue = 0
-            }
-        }
+		const difference = bigInt(calcHash).xor(calcHash2);
 
-        return hashString
-    }
+		return difference.toString(2).split('1').length - 1;
+	}
 
-    public compareHash(hash: string, hash2: string): number {
-        const calcHash = this._hex2Dec(hash)
-        const calcHash2 = this._hex2Dec(hash2)
+	public async distance(imagePath: string | Buffer, image2: string | Buffer, humanize = false): Promise<DistanceResult> {
+		const firstHash = await this.execute(imagePath);
+		const secondHash = await this.execute(image2);
 
-        const difference = bigInt(calcHash).xor(calcHash2)
+		const dist = this.compareHash(firstHash, secondHash);
 
-        return difference.toString(2).split("1").length - 1
-    }
+		return {
+			distance: dist,
+			hashes: {
+				hashA: firstHash,
+				hashB: secondHash,
+			},
+		};
+	}
 
-    public async compare(imagePath: string | Buffer, image2: string | Buffer, options?: ComparisonOptions): Promise<ComparisonResult>{
-        const firstHash = await this.calculate(imagePath)
-        const secondHash = await this.calculate(image2)
-    
-        const dist = this.compareHash(firstHash, secondHash)
-    
-        return {distance: dist, hashes: {image: firstHash, image2: secondHash}}
-    }
+	private async _difference(image: string | Buffer): Promise<boolean[]> {
+		const baseWidth = this.width;
+		const baseHeight = this.height;
+		const baseBuffer = typeof image === 'string' ? readFileSync(image) : image;
 
-    private async _difference(image: string | Buffer): Promise<boolean[]> {
-        const baseWidth = this.width
-        const baseHeight = this.height
-        const baseBuffer = typeof image == "string" ? fs.readFileSync(image) : image;
+		const treatedImage = await sharp(baseBuffer)
+			.resize(baseWidth, baseHeight) // 9x8 = 72 pixels
+			.grayscale()
+			.raw()
+			.toBuffer();
 
-        const treatedImage = await sharp(baseBuffer)
-            .resize(baseWidth, baseHeight) // 9x8 = 72 pixels
-            .grayscale()
-            .raw()
-            .toBuffer()
+		const pixels = new Int32Array(treatedImage);
+		const difference = [];
 
-        const pixels = new Int32Array(treatedImage)
-        let difference = []
+		for (let i = 0; i < baseHeight; i++) {
+			const startRow = i * baseWidth;
+			for (let j = 0; j < baseWidth - 1; j++) {
+				const leftPixelIndex = startRow + j;
+				difference.push(pixels[leftPixelIndex] > pixels[leftPixelIndex + 1]);
+			}
+		}
 
-        for (let i = 0; i < baseHeight; i++) {
-            let startRow = i * baseWidth
-            for (let j = 0; j < baseWidth - 1; j++) {
-                let left_pixel_index = startRow + j
-                difference.push(pixels[left_pixel_index] > pixels[left_pixel_index + 1])
-            }
-        }
+		return difference;
+	}
 
-        return difference
-    };
+	private _hex2Dec(hex: string) {
+		if (hex.length % 2) {
+			hex = '0' + hex;
+		}
 
-    private _hex2Dec(hex: string) {
-        if (hex.length % 2) {
-            hex = '0' + hex;
-        }
-        const bigNumber = BigInt('0x' + hex)
-        return bigNumber.toString(10)
-    }
+		const bigNumber = BigInt('0x' + hex);
+		return bigNumber.toString(10);
+	}
 }
--- src/structures/pHash.ts
+++ src/structures/pHash.ts
@@ -1,121 +1,146 @@
-import { readFileSync } from 'fs';
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/restrict-plus-operands */
+
+import type {ComparisonOptions} from '../typing';
+import type {DistanceResult} from '../typing/structures';
+
+import {readFileSync} from 'fs';
 import sharp from 'sharp';
 
-export default class pHash {
-    
-    private size: number;
-    private low_size: number;
+export default class PerceptualHash {
+	private readonly size: number;
+	private readonly lowSize: number;
 
-    constructor() {
-        this.size = 32;
-        this.low_size = 8;
-    }
+	constructor() {
+		this.size = 32;
+		this.lowSize = 8;
+	}
 
-    public async execute (image: string | Buffer) {
-        let _image = typeof image == "string"
-            ? readFileSync(image) : image;
+	public async execute(image: Buffer) {
+		image = await sharp(image)
+			.grayscale()
+			.resize(this.size, this.size, {fit: 'fill'})
+			.rotate()
+			.raw()
+			.toBuffer();
 
-        _image = await sharp(_image)
-            .grayscale()
-            .resize(this.size, this.size, { fit: 'fill' })
-            .rotate()
-            .raw()
-            .toBuffer();
+		const signal = new Array(this.size);
 
-        //const pixels = new Int32Array(_image);
-        const signal = new Array(this.size);
-        
-        for (let x = 0; x < this.size; x++) {
-          signal[x] = new Array(this.size);
-          for (let y = 0; y < this.size; y++) {
-            signal[x][y] = _image[this.size * y + x]
-          }
-        }
+		for (let x = 0; x < this.size; x++) {
+			signal[x] = new Array(this.size);
+			for (let y = 0; y < this.size; y++) {
+				signal[x][y] = image[this.size * (y + x)];
+			}
+		}
 
-        const sqrt = this._setSQRT();
-        const cosine = this._setCosine();
+		const sqrt = this._setSqrt();
+		const cosine = this._setCosine();
 
-        const dct = this._initDCT(signal, cosine, sqrt);
-        
-        let finalSum = 0;
+		const dct = this._initDct(signal, cosine, sqrt);
 
-        for (let x = 0; x < this.low_size; x++) {
-          for (let y = 0; y < this.low_size; y++) {
-            finalSum += dct[x + 1][y + 1];
-          }
-        }
+		let finalSum = 0;
 
-        const average = finalSum / (this.low_size * this.low_size);
+		for (let x = 0; x < this.lowSize; x++) {
+			for (let y = 0; y < this.lowSize; y++) {
+				finalSum += dct[x + 1][y + 1];
+			}
+		}
 
-        let hash = "";
+		const average = finalSum / (this.lowSize * this.lowSize);
 
-        for (let x = 0; x < this.low_size; x++) {
-          for (let y = 0; y < this.low_size; y++) {
-            hash += dct[x + 1][y + 1] > average ? "1" : "0";
-          }
-        }
+		let hash = '';
 
-        console.log(hash);
-        return hash;
-    }
+		for (let x = 0; x < this.lowSize; x++) {
+			for (let y = 0; y < this.lowSize; y++) {
+				hash += dct[x + 1][y + 1] > average ? '1' : '0';
+			}
+		}
 
-    public async distance (a: string, b: string) {
-      let count = 0;
-      for (let i = 0; i < a.length; i++) {
-        if (a[i] !== b[i]) {
-          count++;
-        }
-      }
-      return count;
-    }
+		return hash;
+	}
 
-    private _setCosine (): number[] {
-        const size = this.size;
-        const cosArray = new Array(size);
+	public async distance(imageA: Buffer, imageB: Buffer, humanize: boolean): Promise<DistanceResult> {
+		const hash1 = await this.execute(imageA);
+		const hash2 = await this.execute(imageB);
 
-        for (let i = 0; i < size; i++) {
-            cosArray[i] = new Array(size);
-          for (let n = 0; n < size; n++) {
-            cosArray[i][n] = Math.cos(((2 * i + 1) / (2.0 * size)) * n * Math.PI);
-          }
-        }
-        
-        return cosArray;
-    };
+		const _distance = this.distanceHash(hash1, hash2);
 
-    private _setSQRT (): number[] {
-        const size = this.size;
-        const c = new Array(size);
-        
-        for (let i = 1; i < size; i++) {
-          c[i] = 1;
-        }
-        
-        c[0] = 1 / Math.sqrt(2.0);
-        
-        return c;
-    };
+		const result: DistanceResult = {distance: _distance, hashes: {hashA: hash1, hashB: hash2}};
 
-    private _initDCT (signal: number[][], cos: any, sqrt: number[]) {
-        
-        const size = this.size;
-        const sampleArr = new Array(size);
-        
-        console.log(size, sampleArr)
+		if (humanize) {
+			if (_distance === 0) {
+				result.distance = 'identical';
+			} else if (_distance > 0 && _distance < 5) {
+				result.distance = 'high similarity';
+			} else if (_distance > 5 && _distance < 10) {
+				result.distance = 'low similarity';
+			} else {
+				result.distance = 'completely different';
+			}
+		}
 
-        for (let u = 0; u < size; u++) {
-          sampleArr[u] = new Array(size);
-          for (let v = 0; v < size; v++) {
-            let sum = 0;
-            for (let i = 0; i < size; i++) {
-              for (let j = 0; j < size; j++) {
-                sum += cos[i][u] * cos[j][v] * signal[i][j];
-              }
-            }
-            sum *= (sqrt[u] * sqrt[v]) / 4;
-            sampleArr[u][v] = sum;
-          }
-        }
-        return sampleArr;
-    }
+		return result;
+	}
+
+	public distanceHash(a: string, b: string) {
+		let count = 0;
+		for (let i = 0; i < a.length; i++) {
+			if (a[i] !== b[i]) {
+				count++;
+			}
+		}
+
+		return count;
+	}
+
+	private _setCosine(): number[] {
+		const {size} = this;
+		const cosArray = new Array(size);
+
+		for (let i = 0; i < size; i++) {
+			cosArray[i] = new Array(size);
+			for (let n = 0; n < size; n++) {
+				cosArray[i][n] = Math.cos(((2 * (i + 1)) / (2.0 * size)) * n * Math.PI);
+			}
+		}
+
+		return cosArray;
+	}
+
+	private _setSqrt(): number[] {
+		const {size} = this;
+		const c = new Array(size);
+
+		for (let i = 1; i < size; i++) {
+			c[i] = 1;
+		}
+
+		c[0] = 1 / Math.sqrt(2.0);
+
+		return c;
+	}
+
+	private _initDct(signal: number[][], cos: any, sqrt: number[]) {
+		const {size} = this;
+		const sampleArr = new Array(size);
+
+		console.log(size, sampleArr);
+
+		for (let u = 0; u < size; u++) {
+			sampleArr[u] = new Array(size);
+			for (let v = 0; v < size; v++) {
+				let sum = 0;
+				for (let i = 0; i < size; i++) {
+					for (let j = 0; j < size; j++) {
+						sum += cos[i][u] * cos[j][v] * signal[i][j];
+					}
+				}
+
+				sum *= (sqrt[u] * sqrt[v]) / 4;
+				sampleArr[u][v] = sum;
+			}
+		}
+
+		return sampleArr;
+	}
 }
